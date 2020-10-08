2d00ac749578162044fb6348e3c0a15ba80dd37c
Tadeu Junior <tadeu_junior@outlook.com>
2020-10-07 22:40:52 -0300

    deploy

---
+++ precache-manifest.14112035c765b939c4c3e90a2cd66f8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7542aec78432734a5269c06ef66c7d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d3ccd35cbc680b608d3",
+    "url": "/static/css/main.06d32c36.chunk.css"
+  },
+  {
+    "revision": "bca943ba961abb08ebbd",
+    "url": "/static/js/2.feaa8828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.feaa8828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3ccd35cbc680b608d3",
+    "url": "/static/js/main.0c31f912.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4f6669bea244ad6c2617d18fcb21921b",
+    "url": "/static/media/logodotei.4f6669be.png"
+  },
+  {
+    "revision": "db9c6cf89fe927ea9abc72bf426f19a9",
+    "url": "/static/media/viralataCaramel1.db9c6cf8.jpg"
+  }
+]);
